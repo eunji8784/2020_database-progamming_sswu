ecbe14adddc2a07c42cb278b854f3034ac942b32
eunji8784 <70746467+eunji8784@users.noreply.github.com>
2020-09-28 22:23:41 +0900

    Add files via upload

---
+++ W04-P/create.php
@@ -0,0 +1,54 @@
+<?php
+  $link = mysqli_connect('localhost', 'root', 'rootroot', 'dbp');
+  $query = "SELECT * FROM food";
+  $result = mysqli_query($link, $query);
+  $list ='';
+
+  while ($row = mysqli_fetch_array($result)) {
+      $list = $list."<li><a href=\"index.php?id={$row['id']}\">{$row['title']}</a></li>";
+  }
+
+  $article = array(
+    'title' => 'Welcome',
+    'description' => 'My favorite food is ...'
+  );
+
+  $updated_link = '';
+
+  if (isset($_GET['id'])) {
+      $query = "SELECT * FROM food WHERE id={$_GET['id']}";
+      $result = mysqli_query($link, $query);
+      $row = mysqli_fetch_array($result);
+      $article = array(
+      'title' => $row['title'],
+      'description' => $row['description']
+    );
+      $updated_link = '<a href="update.php?id'.$_GET['id'].'">update</a>';
+  }
+
+  $query = "SELECT * FROM author";
+  $result = mysqli_query($link, $query);
+  $select_form = '<select name="author_id">';
+  while ($row = mysqli_fetch_array($result)) {
+    $select_form .= '<option value="'.$row['id'].'">'.$row['name'].'</option>';
+  }
+  $select_form .= '</select>';
+ ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>My favorite food is</title>
+  </head>
+  <body>
+    <h1 style="background-color:orange;"><a href="index.php">My favorite food is</a></h1>
+    <ol><?= $list ?></ol>
+    <form action="process_create.php" method="POST">
+      <p><input type="text" name="title" placeholder="title"></p>
+      <p><textarea name="description" placeholder="description"></textarea></p>
+      <?= $select_form ?>
+      <p><input type="submit"></p>
+    </form>
+  </body>
+</html>
---
+++ W04-P/update.php
@@ -0,0 +1,45 @@
+<?php
+  $link = mysqli_connect('localhost','root','rootroot','dbp');
+  $query = 'SELECT * FROM food';
+  $result = mysqli_query($link, $query);
+  $list = '';
+  while($row = mysqli_fetch_array($result)){
+    $list = $list."<li><a href=\"index.php?id={$row['id']}\">{$row['title']}</a></li>";
+  }
+
+  $article = array(
+    'title' => 'Welcome',
+    'description' => 'My favorite food is ...'
+  );
+
+  if( isset($_GET['id']) ) {
+    $filtered_id = mysqli_real_escape_string($link, $_GET['id']);
+    $query = "SELECT * FROM food WHERE id={$filtered_id}";
+    $result = mysqli_query($link, $query);
+    $row = mysqli_fetch_array($result);
+    $article = array(
+      'title' => $row['title'],
+      'description' => $row['description']
+    );
+  }
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title> My favorite food is </title>
+</head>
+<body>
+  <h1 style="background-color:orange;"><a href="index.php">  My favorite food is </a></h1>
+  <ol> <?= $list ?> </ol>
+  <form action="process_update.php" method="post">
+    <input type="hidden" name="id" value="<?=$filtered_id?>">
+    <p><input type="text" name="title" placeholder="title"
+    value="<?=$article['title']?>"></p>
+    <p><textarea name="description"
+    placeholder="description"><?=$article['description']?></textarea></p>
+    <p><input type="submit"></p>
+  </form>
+</body>
+</html>
